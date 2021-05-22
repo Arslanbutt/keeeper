fae1459a239b85a5174f8c38e92545cc3cc99a90
Arslan <arslanbutt7545@gmail.com>
2021-05-22 11:54:12 +0500

    Add Note component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./header";
 import Footer from "./footer";
+import Note from "./note";
 
 function app(){
     return <div>
         <Header/>
+        <Note/>
         <Footer/>
     </div>
 }
