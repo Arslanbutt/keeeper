59d1ce9e836be2ee2fcc3179f0fcad73bfc6597e
Arslan <arslanbutt7545@gmail.com>
2021-05-22 11:50:49 +0500

    Add Footer component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./header";
+import Footer from "./footer";
 
 function app(){
     return <div>
         <Header/>
+        <Footer/>
     </div>
 }
 
