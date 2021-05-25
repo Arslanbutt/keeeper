e0269559a964380d5202bca7d21156f4906803e4
Arslan <arslanbutt7545@gmail.com>
2021-05-25 22:01:06 +0500

    Component generation through map and data passing through props

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
+import notes from "../notes";
+
 
 function app(){
     return <div>
         <Header/>
-        <Note/>
+        {notes.map(note => (<Note title={note.title} content={note.content}/>))}
         <Footer/>
     </div>
 }
